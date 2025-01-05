bbe1ce2a88674dbdce0b552a5787f391c6c9a0f8
vineet kumar <vineetkumar@vineet-ka-MacBook-Air.local>
2025-01-06 00:26:51 +0530

    feature(): in dev

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
+export default async function Page() {
+    let myFunc = async() => {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+
+    await myFunc()
     return <p>Customers Page</p>;
   }
